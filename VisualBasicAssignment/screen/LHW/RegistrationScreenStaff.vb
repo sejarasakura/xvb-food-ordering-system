5bd22e49c5a460227c3717d1b666961d0227b2cb
hongwah <73833818+hongwah@users.noreply.github.com>
2021-03-11 13:25:45 +0800

    register

--- VisualBasicAssignment/screen/LHW/RegistrationScreenStaff.vb
+++ VisualBasicAssignment/screen/LHW/RegistrationScreenStaff.vb
@@ -1,26 +1,6 @@
 ﻿Public Class RegistrationScreenStaff
-    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
-        txtStaffName.Clear()
-        txtEmail.Clear()
-        txtPosition.Clear()
-        radMale.Checked = False
-        radFemale.Checked = False
-        txtAddress.Clear()
-        txtSalary.Clear()
-        txtMobileNumber.Clear()
-        radSingle.Checked = False
-        radMarried.Checked = False
-    End Sub
-
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
         Me.Close()
     End Sub
 
-    Private Sub lblPosition_Click(sender As Object, e As EventArgs) Handles lblRole.Click
-
-    End Sub
-
-    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
-
-    End Sub
 End Class
