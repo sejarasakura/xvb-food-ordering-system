161b8f69bd5ef8026379ad5f11e4f53dfebfacda
wang3081 <77950891+wang3081@users.noreply.github.com>
2021-03-15 09:23:52 +0800

    2
    44

---
+++ VisualBasicAssignment/form/Profile.vb
@@ -0,0 +1,13 @@
+﻿Public Class Profile
+    Private Sub ProfileScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtName.Clear()
+        txtPassowrd.Clear()
+        txtConfirmPassword.Clear()
+        txtEmail.Clear()
+        txtHPNumber.Clear()
+    End Sub
+End Class
---
+++ VisualBasicAssignment/form/Register.Designer.vb
@@ -0,0 +1,202 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Register
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnCreate = New Guna.UI2.WinForms.Guna2Button()
+        Me.txtConfirmPassword = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.txtPassword = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.txtEmail = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.txtUserName = New Guna.UI2.WinForms.Guna2TextBox()
+        Me.Guna2AnimateWindow1 = New Guna.UI2.WinForms.Guna2AnimateWindow(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.White
+        Me.GroupBox1.Controls.Add(Me.btnCreate)
+        Me.GroupBox1.Controls.Add(Me.txtConfirmPassword)
+        Me.GroupBox1.Controls.Add(Me.txtPassword)
+        Me.GroupBox1.Controls.Add(Me.txtEmail)
+        Me.GroupBox1.Controls.Add(Me.txtUserName)
+        Me.GroupBox1.Font = New System.Drawing.Font("Lucida Console", 15.75!)
+        Me.GroupBox1.Location = New System.Drawing.Point(16, 15)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(794, 407)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Registration"
+        '
+        'btnCreate
+        '
+        Me.btnCreate.BackColor = System.Drawing.Color.FromArgb(CType(CType(3, Byte), Integer), CType(CType(155, Byte), Integer), CType(CType(205, Byte), Integer))
+        Me.btnCreate.CheckedState.Parent = Me.btnCreate
+        Me.btnCreate.CustomImages.Parent = Me.btnCreate
+        Me.btnCreate.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.btnCreate.ForeColor = System.Drawing.Color.White
+        Me.btnCreate.HoverState.Parent = Me.btnCreate
+        Me.btnCreate.Location = New System.Drawing.Point(287, 319)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.ShadowDecoration.Parent = Me.btnCreate
+        Me.btnCreate.Size = New System.Drawing.Size(207, 62)
+        Me.btnCreate.TabIndex = 5
+        Me.btnCreate.Text = "Create Account"
+        '
+        'txtConfirmPassword
+        '
+        Me.txtConfirmPassword.BackColor = System.Drawing.Color.Transparent
+        Me.txtConfirmPassword.BorderRadius = 10
+        Me.txtConfirmPassword.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtConfirmPassword.DefaultText = "Confirm Password"
+        Me.txtConfirmPassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.txtConfirmPassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.txtConfirmPassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtConfirmPassword.DisabledState.Parent = Me.txtConfirmPassword
+        Me.txtConfirmPassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtConfirmPassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtConfirmPassword.FocusedState.Parent = Me.txtConfirmPassword
+        Me.txtConfirmPassword.Font = New System.Drawing.Font("Segoe UI Semilight", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtConfirmPassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtConfirmPassword.HoverState.Parent = Me.txtConfirmPassword
+        Me.txtConfirmPassword.Location = New System.Drawing.Point(149, 233)
+        Me.txtConfirmPassword.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtConfirmPassword.Name = "txtConfirmPassword"
+        Me.txtConfirmPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txtConfirmPassword.PlaceholderText = ""
+        Me.txtConfirmPassword.SelectedText = ""
+        Me.txtConfirmPassword.SelectionStart = 16
+        Me.txtConfirmPassword.ShadowDecoration.Parent = Me.txtConfirmPassword
+        Me.txtConfirmPassword.Size = New System.Drawing.Size(465, 51)
+        Me.txtConfirmPassword.TabIndex = 4
+        '
+        'txtPassword
+        '
+        Me.txtPassword.BackColor = System.Drawing.Color.Transparent
+        Me.txtPassword.BorderRadius = 10
+        Me.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtPassword.DefaultText = "Password"
+        Me.txtPassword.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.txtPassword.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.txtPassword.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtPassword.DisabledState.Parent = Me.txtPassword
+        Me.txtPassword.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtPassword.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtPassword.FocusedState.Parent = Me.txtPassword
+        Me.txtPassword.Font = New System.Drawing.Font("Segoe UI Semilight", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtPassword.HoverState.Parent = Me.txtPassword
+        Me.txtPassword.Location = New System.Drawing.Point(149, 167)
+        Me.txtPassword.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txtPassword.PlaceholderText = ""
+        Me.txtPassword.SelectedText = ""
+        Me.txtPassword.SelectionStart = 8
+        Me.txtPassword.ShadowDecoration.Parent = Me.txtPassword
+        Me.txtPassword.Size = New System.Drawing.Size(465, 51)
+        Me.txtPassword.TabIndex = 3
+        '
+        'txtEmail
+        '
+        Me.txtEmail.BackColor = System.Drawing.Color.Transparent
+        Me.txtEmail.BorderRadius = 10
+        Me.txtEmail.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtEmail.DefaultText = "Email"
+        Me.txtEmail.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.txtEmail.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.txtEmail.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtEmail.DisabledState.Parent = Me.txtEmail
+        Me.txtEmail.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtEmail.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtEmail.FocusedState.Parent = Me.txtEmail
+        Me.txtEmail.Font = New System.Drawing.Font("Segoe UI Semilight", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtEmail.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtEmail.HoverState.Parent = Me.txtEmail
+        Me.txtEmail.Location = New System.Drawing.Point(149, 101)
+        Me.txtEmail.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txtEmail.PlaceholderText = ""
+        Me.txtEmail.SelectedText = ""
+        Me.txtEmail.SelectionStart = 5
+        Me.txtEmail.ShadowDecoration.Parent = Me.txtEmail
+        Me.txtEmail.Size = New System.Drawing.Size(465, 51)
+        Me.txtEmail.TabIndex = 2
+        '
+        'txtUserName
+        '
+        Me.txtUserName.BackColor = System.Drawing.Color.Transparent
+        Me.txtUserName.BorderRadius = 10
+        Me.txtUserName.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txtUserName.DefaultText = "Username"
+        Me.txtUserName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer), CType(CType(208, Byte), Integer))
+        Me.txtUserName.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer), CType(CType(226, Byte), Integer))
+        Me.txtUserName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtUserName.DisabledState.Parent = Me.txtUserName
+        Me.txtUserName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(138, Byte), Integer))
+        Me.txtUserName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtUserName.FocusedState.Parent = Me.txtUserName
+        Me.txtUserName.Font = New System.Drawing.Font("Segoe UI Semilight", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUserName.HoverState.BorderColor = System.Drawing.Color.FromArgb(CType(CType(94, Byte), Integer), CType(CType(148, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtUserName.HoverState.Parent = Me.txtUserName
+        Me.txtUserName.Location = New System.Drawing.Point(149, 33)
+        Me.txtUserName.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtUserName.Name = "txtUserName"
+        Me.txtUserName.PasswordChar = Global.Microsoft.VisualBasic.ChrW(0)
+        Me.txtUserName.PlaceholderText = ""
+        Me.txtUserName.SelectedText = ""
+        Me.txtUserName.SelectionStart = 8
+        Me.txtUserName.ShadowDecoration.Parent = Me.txtUserName
+        Me.txtUserName.Size = New System.Drawing.Size(465, 51)
+        Me.txtUserName.TabIndex = 1
+        '
+        'Register
+        '
+        Me.AcceptButton = Me.btnCreate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(830, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Register"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Register"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtUserName As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents btnCreate As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents txtConfirmPassword As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents txtPassword As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents txtEmail As Guna.UI2.WinForms.Guna2TextBox
+    Friend WithEvents Guna2AnimateWindow1 As Guna.UI2.WinForms.Guna2AnimateWindow
+End Class
