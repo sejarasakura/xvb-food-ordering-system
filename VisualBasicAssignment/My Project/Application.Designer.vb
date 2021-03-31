0d3d4d3bd2f9727a98c69b66dcd57cc8bd69e286
hongwah <73833818+hongwah@users.noreply.github.com>
2021-04-01 02:01:46 +0800

    register1

--- VisualBasicAssignment/My Project/Application.Designer.vb
+++ VisualBasicAssignment/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.VisualBasicAssignment.Login
+            Me.MainForm = Global.VisualBasicAssignment.Register
         End Sub
     End Class
 End Namespace
--- VisualBasicAssignment/screen/LHW/Register.vb
+++ VisualBasicAssignment/screen/LHW/Register.vb
@@ -1,4 +1,6 @@
 ﻿Public Class Register
+    Dim err As New ErrorProvider
+
     Private Sub RegistrationScreenCustomer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -20,7 +22,51 @@
     End Sub
 
     Private Sub btnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
-        Me.Hide()
-        Profile.Show()
+        Dim strName As String = txtUserName.Text()
+        Dim strEmail As String = txtEmail.Text()
+        Dim strPassword As String = txtPassword.Text()
+        Dim strCPassword As String = txtConfirmPassword.Text()
+
+        Err.Tag = Nothing
+
+        If strName = "" Then
+            err.SetError(txtUserName, "Please type [Username]")
+            err.Tag = If(err.Tag, txtUserName)
+        Else
+            err.SetError(txtUserName, Nothing)
+        End If
+        If strEmail = "" Then
+            err.SetError(txtEmail, "Please type [Email]")
+            err.Tag = If(Err.Tag, txtEmail)
+        Else
+            err.SetError(txtEmail, Nothing)
+        End If
+        If strPassword = "" Then
+            err.SetError(txtPassword, "Please type [Password]")
+            err.Tag = If(Err.Tag, txtPassword)
+        Else
+            err.SetError(txtPassword, Nothing)
+        End If
+        If strCPassword = "" Then
+            err.SetError(txtConfirmPassword, "Please type [Confirm Password]")
+            err.Tag = If(err.Tag, txtConfirmPassword)
+        Else
+            err.SetError(txtConfirmPassword, Nothing)
+        End If
+        If Err.Tag IsNot Nothing Then
+            CType(Err.Tag, Control).Focus()
+            Return
+        End If
+
+        Dim c As New Customer()
+        c.cust_name = strName
+        c.cust_email = strEmail
+        c.cust_password = strPassword
+
+        Dim db As New FoodShopDataContext()
+        db.Customers.InsertOnSubmit(c)
+        db.SubmitChanges()
+
+        MessageBox.Show("Successfully Registered")
     End Sub
 End Class
