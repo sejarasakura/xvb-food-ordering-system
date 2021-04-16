83a2f6451d83d05919e2888e33eeef0a5de6560a
Lim Sai Keat <saikeat.lim@mycareconcierge.com>
2021-04-16 21:48:43 +0800

    update

--- VisualBasicAssignment/screen/Screen.vb
+++ VisualBasicAssignment/screen/Screen.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Screen
-    Friend Shared ReadOnly NAV_LOGIN_SCREEN As LoginScreen = New LoginScreen()
+    Friend Shared ReadOnly NAV_LOGIN_SCREEN As Login = New Login()
     Friend Shared ReadOnly NAV_HOME_SCREEN As HomeScreen = New HomeScreen()
     Friend Shared ReadOnly NAV_PAYMENT_SCREEN As PaymentScreen = New PaymentScreen()
     Friend Shared ReadOnly NAV_ORDER_SCREEN As OrderScreen = New OrderScreen()
